72852f0acab65f6b4c772b88f6fea485689b92a0
Giang Tran <119998442+Yangchann@users.noreply.github.com>
2024-07-13 22:03:46 +0700

    Delete module_2/week_1/week_1.py

--- module_2/week_1/week_1.py
+++
@@ -1,6 +0,0 @@
-! gdown 1 i9dqan21DjQoG5Q_VEvm0LrVwAlXD0vB
-3
-4 import matplotlib . image as mpimg
-5 img = mpimg . imread (’/ content /dog. jpeg ’)
-6 gray_img_01 = # Your Code Here
-7 gray_img_01 [0 , 0]
